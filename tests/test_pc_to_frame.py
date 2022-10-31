2b26f0c609fb0de311c33c633854e765794d717d
Nicola Piga <nicolapiga@gmail.com>
2022-10-31 17:36:37 +0100

    Show object reference frame given by Open3D OrientedBoundingBox

--- tests/test_pc_to_frame.py
+++ tests/test_pc_to_frame.py
@@ -1,5 +1,8 @@
+import time
 import numpy
 import open3d as o3d
+from sklearn.decomposition import PCA
+
 
 def add_point_cloud(name, points, color, size, scene):
 
@@ -14,10 +17,38 @@ def add_point_cloud(name, points, color, size, scene):
     scene.add_geometry(name, cloud, material)
 
 
+def add_axes(name, pose, scene):
+
+    frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size = 0.05)
+    frame.transform(pose)
+
+    material = o3d.visualization.rendering.MaterialRecord()
+    material.shader = 'defaultUnlit'
+
+    scene.add_geometry(name, frame, material)
+
+
+def get_pose(points):
+
+    cloud = o3d.geometry.PointCloud()
+    cloud.points = o3d.utility.Vector3dVector(points)
+
+    center = cloud.get_center()
+    bbox = cloud.get_oriented_bounding_box()
+    pose = numpy.eye(4)
+    pose[0:3, 0:3] = bbox.R
+    pose[0:3, 3] = center
+
+    return pose
+
+
 def main():
     cloud = numpy.loadtxt('./reconstruction.txt')
+
+    pose = get_pose(cloud)
+
     centroid = numpy.zeros((1, 3))
-    centroid[0, :] = numpy.mean(cloud, axis = 0)
+    centroid[0, :] = pose[0:3, 3]
 
     try:
         app = o3d.visualization.gui.Application.instance
@@ -29,9 +60,10 @@ def main():
         window.add_child(widget3d)
 
         add_point_cloud('object', cloud, [33 / 255, 150 / 255, 243 / 255], 3, widget3d.scene)
-        add_point_cloud('centroid', centroid, [1, 0, 0], 10,  widget3d.scene)
-
+        add_point_cloud('centroid', centroid, [1, 0, 0], 10, widget3d.scene)
         widget3d.setup_camera(60, widget3d.scene.bounding_box, [0, 0, 0])
+        add_axes('camera_axes', numpy.eye(4), widget3d.scene)
+        add_axes('object_axes', pose, widget3d.scene)
 
         app.run()
     except Exception as e:
