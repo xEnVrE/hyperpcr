179fa16fbacc55bba87ec5bf70287331abf3b750
Nicola Piga <nicolapiga@gmail.com>
2022-11-17 14:03:02 +0000

    Implement Config.__str__ in pcr_yarp/config.py

--- pcr_yarp/config.py
+++ pcr_yarp/config.py
@@ -30,3 +30,28 @@ class Config:
 
     class PoseFiltering:
         enable = bool(True)
+
+    def __str__(self):
+
+        c = self.Camera
+        d = self.DBSCAN
+        de = self.Depth
+        i = self.Input
+        m = self.Module
+        o = self.Open3D
+        p = self.PoseFiltering
+
+        return 'Camera parameters are:\r\n' + \
+               '    w = ' + str(c.width) + ', h = ' + str(c.height) + ', fx = ' + str(c.fx) + ', fy = ' + str(c.fy) + ', cx = ' + str(c.cx) + ', cy = ' + str(c.cy) + '\r\n\r\n' + \
+               'DBSCAN parameters are:\r\n' + \
+               '    enable = ' + str(d.enable) + ', eps = ' + str(d.eps) + ', min_samples = ' + str(d.min_samples) + ', use_cuml = ' + str(d.use_cuml) + '\r\n\r\n' + \
+               'Depth parameters are:\r\n' + \
+               '    lower_bound = ' + str(de.lower_bound) + ', upper_bound = ' + str(de.upper_bound) + '\r\n\r\n' + \
+               'Input paramters are:\r\n' + \
+               '    joint_input_mode = ' + str(i.joint_input_mode) + '\r\n\r\n' + \
+               'Module parameters are:\r\n' + \
+               '    gpu_id = ' + str(m.gpu_id) + ', period = ' + str(m.period) + '\r\n\r\n' + \
+               'Open3D parameters are:\r\n' + \
+               '    oriented_bounding_box_robust = ' + str(o.oriented_bounding_box_robust) + '\r\n\r\n' + \
+               'Pose filtering parameters are:\r\n' + \
+               '    enable = ' + str(p.enable) + '\r\n'
